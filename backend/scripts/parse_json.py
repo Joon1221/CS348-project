a678e2d811aaee63b07df6cf78a60727d404dca1
JF236 <jadeforrest1@outlook.com>
2024-06-13 17:12:20 -0400

    parsing json

--- backend/scripts/parse_json.py
+++ backend/scripts/parse_json.py
@@ -3,8 +3,9 @@ import requests
 headers = {
     'x-api-key': 'CA63472B098944D98AAF2C426DB49FCB'
 }
+# returns: a comma separated list of tuples of the values from the json file
 
-def get_course(course_id):
+def parse_json(course_id):
   r = requests.get(
     # TODO: add parameter to specify term code
     f'https://openapi.data.uwaterloo.ca/v3/Courses/1245/', headers=headers)
@@ -12,4 +13,4 @@ def get_course(course_id):
   return course[0]
 
 
-print(get_course('006847'))
+print(parse_json('006847'))
