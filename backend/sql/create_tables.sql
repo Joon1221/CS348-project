c4ad8689e78000e9d4d632f5e26764ede5a2002d
Jeannie Q <62075564+jeannie01@users.noreply.github.com>
2024-06-18 22:39:35 -0400

    updating create tables

--- backend/sql/create_tables.sql
+++ backend/sql/create_tables.sql
@@ -1,20 +1,20 @@
-CREATE TABLE Course (
-    units FLOAT NOT NULL,
-    department_name VARCHAR(6) NOT NULL,
+CREATE TABLE IF NOT EXISTS Course (
+    course_id VARCHAR(10) NOT NULL,
+    subject_code VARCHAR(6) NOT NULL,
+    catalog_number VARCHAR(6) NOT NULL,
+    course_name VARCHAR(100) NOT NULL,
     course_desc VARCHAR(1000) NOT NULL,
-    course_code VARCHAR(10) NOT NULL PRIMARY KEY,
-    course_name VARCHAR(100) NOT NULL
+    PRIMARY KEY (subject_code, catalog_number)
 );
-
 CREATE TABLE Antireq(
-    course_code VARCHAR(10) NOT NULL,
-    antireq_code VARCHAR(10) NOT NULL
+    course_id VARCHAR(10) NOT NULL,
+    antireq_id VARCHAR(10) NOT NULL
 );
 
 
 CREATE TABLE Prereq(
-    course_code VARCHAR(10) NOT NULL,
-    antireq_code VARCHAR(10) NOT NULL,
+    course_id VARCHAR(10) NOT NULL,
+    antireq_id VARCHAR(10) NOT NULL,
 );
 
 
@@ -29,21 +29,70 @@ CREATE TABLE Student (
     acad_level VARCHAR(3) -- might be NULL if student is going into first year? idk
 );
 
-CREATE TABLE --incomplete
-
-CREATE TABLE Schedule (
+CREATE TABLE EnrolledIn (
     student_username VARCHAR(8) NOT NULL,
-    section_id VARCHAR(4) NOT NULL,
-    course_code VARCHAR(10) NOT NULL,
+    course_id INT NOT NULL,
+    section_id INT NOT NULL,
+    term INT NOT NULL,
     PRIMARY KEY (student_username, section_id),
     FOREIGN KEY (student_username) REFERENCES Student(username),
-    FOREIGN KEY (section_id) REFERENCES Section(section_id), -- assuming the sections/components table is called "Section"
-    FOREIGN KEY (course_code) REFERENCES Course(course_code)
+    FOREIGN KEY (course_id, section_id, term) REFERENCES Section(course_id, section_id, term)
 );
 
-CREATE TABLE Courses_Taken (
+CREATE TABLE TaughtBy (
+    prof_username VARCHAR(8) NOT NULL,
+    course_id INT NOT NULL,
+    section_id INT NOT NULL,
+    term INT NOT NULL,
+    PRIMARY KEY (prof_username, course_id, section_id, term),
+    FOREIGN KEY (prof_username) REFERENCES Prof(username),
+    FOREIGN KEY (course_id, section_id, term) REFERENCES (course_id, section_id, term)
+);
+
+CREATE TABLE Section (
+    course_id INT NOT NULL REFERENCES Course(course_id),
+    section_number INT NOT NULL,
+    section_id INT NOT NULL,
+    term INT NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    weekdays VARCHAR(10) -- M,T,W,Th,F,Sa,Su
+    component_type VARCHAR(3),
+    currently_enrolled INT NOT NULL,
+    total_cap_size INT NOT NULL,
+    PRIMARY(course_id, section_id, term) 
+);
+
+CREATE TABLE InPersonSection (
+    course_id INT NOT NULL ,
+    section_number INT NOT NULL,
+    section_id INT NOT NULL,
+    term INT NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    weekdays VARCHAR(10) -- M,T,W,Th,F,Sa,Su
+    component_type VARCHAR(3),
+    currently_enrolled INT NOT NULL,
+    total_cap_size INT NOT NULL,
+    FOREIGN KEY(course_id, section_number, section_id, term, start_time, end_time, weekdays, component_type, currently_enrolled,total_cap_size) REFERENCES Section(course_id, section_number, section_id, term, start_time, end_time, weekdays, component_type, currently_enrolled,total_cap_size),
+    Location VARCHAR(50),
+);
+
+CREATE TABLE CurrentSchedule (
     student_username VARCHAR(8) NOT NULL,
-    course_code VARCHAR(10) NOT NULL,
-    PRIMARY KEY (student_username, course_code),
-    FOREIGN KEY (course_code) REFERENCES Course(course_code)
+    course_id INT NOT NULL,
+    section_id INT NOT NULL,
+    term INT NOT NULL,
+    course_id VARCHAR(10) NOT NULL,
+    PRIMARY KEY (student_username, section_id),
+    FOREIGN KEY (student_username) REFERENCES Student(username),
+    FOREIGN KEY (course_id, section_id, term) REFERENCES Section(course_id, section_id, term)
+    FOREIGN KEY (course_id) REFERENCES Course(course_id)
+);
+
+CREATE TABLE CoursesTaken (
+    student_username VARCHAR(8) NOT NULL,
+    course_id VARCHAR(10) NOT NULL,
+    PRIMARY KEY (student_username, course_id),
+    FOREIGN KEY (course_id) REFERENCES Course(course_id)
 );
