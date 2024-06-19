28808dec4e3765a34486627b7b27c63c999e0354
Joon1221 <joonkang940@gmail.com>
2024-06-19 18:04:58 -0400

    create test samples

---
+++ backend/sql/test-sample.sql
@@ -0,0 +1,6 @@
+-- the call to these SQL queries are located in backend/api/views.py
+INSERT INTO CurrentSchedule (course_id) VALUES ('MATH 135');
+INSERT INTO CurrentSchedule (course_id) VALUES ('SE 463');
+SELECT * FROM CurrentSchedule;
+
+SELECT * FROM Course LIMIT 10;
