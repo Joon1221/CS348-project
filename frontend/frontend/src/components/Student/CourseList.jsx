da9bf857f89070f36aa942064ea747b1700acd57
Grace Song <gracehsong1@gmail.com>
2024-07-14 22:42:30 -0400

    Add dropdown search filter
    Add dropdown filter with search bar for CourseList.jsx

--- frontend/frontend/src/components/Student/CourseList.jsx
+++ frontend/frontend/src/components/Student/CourseList.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "@mui/material/Button";
 import styled from "styled-components";
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 const Title = styled.h1`
   text-align: center;
@@ -10,7 +12,10 @@ const listOfItems = (items) => {
   return items.map((course, i) => <li key={i}>{course}</li>);
 };
 
+
 export default function CourseList({ courses, getAllCourses }) {
+  const [filteredCourses, setFilteredCourses] = useState(courses);
+
   return (
     <>
       <Button variant="outlined" onClick={getAllCourses}>
@@ -19,7 +24,32 @@ export default function CourseList({ courses, getAllCourses }) {
       {courses.length > 0 && (
         <>
           <Title>Top 10 Courses</Title>
-          <ul>{listOfItems(courses)}</ul>
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={courses}
+            onChange={(event, newValue) => {
+              if (newValue.length == 0) setFilteredCourses(courses);
+              else setFilteredCourses(newValue);
+            }}
+            // getOptionLabel={(option) => option.title}
+            // defaultValue={[courses[13]]}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Department"
+                placeholder="E.g. MATH"
+              />
+            )}
+          />
+          <>
+          {filteredCourses.map(courses => (
+            <div key={courses}>
+              <p>{courses}</p>
+            </div>
+          ))}
+          </>
         </>
       )}
     </>
