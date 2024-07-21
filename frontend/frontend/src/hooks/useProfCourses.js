8a6c9ad7b7ffe5b6050a2e912437313cb323dcd0
Jeannie Q <62075564+jeannie01@users.noreply.github.com>
2024-07-21 16:14:12 -0400

    prof course backend functionality

---
+++ frontend/frontend/src/hooks/useProfCourses.js
@@ -0,0 +1,64 @@
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
+const useProfCourse = (username) => {
+  const [profCoursesTaught, setProfCoursesTaught] = useState([]);
+
+  const getAllProfCoursesTaught = useCallback(() => {
+    axios
+      .get("http://localhost:8000/api/get_professor_course_taught/", {
+        params: { username },
+      })
+      .then((response) => {
+        // console.log("tada", response.data.message);
+        setProfCoursesTaught(response.data.message);
+      })
+      .catch((error) => {
+        console.log(`Get User Courses Error: ${error}`);
+      });
+  }, [username]);
+
+  const addProfCourseTaught = ({ subject_code, catalog_number }) => {
+    axios
+      .put("http://localhost:8000/api/put_professor_course_taught/", {
+        username: username,
+        subject_code: subject_code,
+        catalog_number: catalog_number,
+      })
+      .then((response) => {
+        console.log(`Response:`, response.data.message);
+        getAllProfCoursesTaught();
+      })
+      .catch((error) => {
+        console.log(`Add Course Error: ${error}`);
+      });
+  };
+
+  const deleteProfCourseTaught = ({ subject_code, catalog_number }) => {
+    axios
+      .put("http://localhost:8000/api/delete_professor_course_taught/", {
+        username: username,
+        subject_code: subject_code,
+        catalog_number: catalog_number,
+      })
+      .then((response) => {
+        console.log(`Response:`, response.data.message);
+        getAllProfCoursesTaught();
+      })
+      .catch((error) => {
+        console.log(`Add Course Error: ${error}`);
+      });
+  };
+
+  useEffect(() => {
+    getAllProfCoursesTaught();
+  }, [getAllProfCoursesTaught]);
+
+  return {
+    profCoursesTaught,
+    addProfCourseTaught,
+    deleteProfCourseTaught,
+  };
+};
+
+export default useProfCourse;
