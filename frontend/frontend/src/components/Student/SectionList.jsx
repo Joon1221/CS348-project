f935b729167b2adcbecb9930217fa41c21c25941
Jeannie Q <62075564+jeannie01@users.noreply.github.com>
2024-07-22 22:36:51 -0400

    prepping get all sections

--- frontend/frontend/src/components/Student/SectionList.jsx
+++ frontend/frontend/src/components/Student/SectionList.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAllSections } from "../../hooks/getDeptInfo";
 
 export default function SectionList() {
+  const [sections, setSections] = useState([]);
+
+  useEffect(() => {
+    const fetchSections = async () => {
+      const result = await getAllSections();
+      console.log("RESULT", result);
+    };
+
+    fetchSections();
+  }, []);
+
   return (
-   <h1>Section List</h1>
+    <>
+      <h1>Section List</h1>
+      <p>Section list content</p>
+    </>
   );
 }
--- frontend/frontend/src/hooks/getDeptInfo.js
+++ frontend/frontend/src/hooks/getDeptInfo.js
@@ -26,3 +26,14 @@ export const getAllDeptCourseCode = async (subject_code) => {
     console.error(`Get all department error:${error}`);
   }
 };
+
+export const getAllSections = async () => {
+  try {
+    const response = await axios.get(
+      "http://localhost:8000/api/get_all_sections/"
+    );
+    return response.data.message;
+  } catch (error) {
+    console.error(`Get all sections error:${error}`);
+  }
+};
